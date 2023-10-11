7294a4fde568538dc54a11ae3d8eea4438a2c239
raschotov <evg.raschotov@gmail.com>
2023-10-11 13:12:31 +0300

    updated to different ack types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.ActiveMQMessageProducer;
-import org.apache.activemq.broker.Broker;
-import org.apache.activemq.broker.BrokerService;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
-import javax.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 
 public class Main {
@@ -14,12 +19,17 @@ public class Main {
     private static final String PASSWORD = "admin";
 
     public static void main(String[] args) throws Exception {
-        /*
-        Broker broker = startEmbeddedBroker().getBroker();
-        System.out.println(broker.getBrokerName());
-         */
-        provider();
-        customer();
+        autoAckProvider();
+        autoAckCustomer();
+
+        clientAckProvider();
+        clientAckCustomer();
+
+        dupsOkAckProvider();
+        dupsOkAckCustomer();
+
+        sessionTransactedAckProvider();
+        sessionTransactedAckCustomer();
     }
 
     public static Connection getConnection() throws JMSException {
@@ -30,15 +40,53 @@ public class Main {
     }
 
     //providers, maybe need to move to separate package
-
     //auto ack non-persistent provider with standard priority
-    private static void provider() {
+    private static void autoAckProvider() {
         try (Connection connection = getConnection()) {
             Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
             Destination destination = session.createQueue("auto ack queue");
             MessageProducer producer = session.createProducer(destination);
             TextMessage message = session.createTextMessage();
-            message.setText("This is test message from .provider()");
+            message.setText("This is auto ack message");
+            producer.send(destination, message, DeliveryMode.NON_PERSISTENT, 4, 1000*10);
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
+    }
+
+    private static void clientAckProvider() {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+            Destination destination = session.createQueue("client ack queue");
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage();
+            message.setText("This is client ack message");
+            producer.send(destination, message, DeliveryMode.NON_PERSISTENT, 4, 1000*10);
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
+    }
+
+    private static void dupsOkAckProvider() {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.DUPS_OK_ACKNOWLEDGE);
+            Destination destination = session.createQueue("dups ok ack queue");
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage();
+            message.setText("This is dups ok message");
+            producer.send(destination, message, DeliveryMode.NON_PERSISTENT, 4, 1000*10);
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
+    }
+
+    private static void sessionTransactedAckProvider() {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+            Destination destination = session.createQueue("session transacted ack queue");
+            MessageProducer producer = session.createProducer(destination);
+            TextMessage message = session.createTextMessage();
+            message.setText("This is session transacted message");
             producer.send(destination, message, DeliveryMode.NON_PERSISTENT, 4, 1000*10);
         } catch (JMSException jmsException) {
             //add log4j error record
@@ -46,9 +94,8 @@ public class Main {
     }
 
     //customers, maybe need to move to separate package
-
     //auto ack customer
-    public static void customer() throws JMSException {
+    public static void autoAckCustomer() throws JMSException {
         try (Connection connection = getConnection()) {
             Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
             Destination destination = session.createQueue("auto ack queue");
@@ -60,14 +107,39 @@ public class Main {
         }
     }
 
-    /*
-    private static BrokerService startEmbeddedBroker() throws Exception {
-        BrokerService brokerService = new BrokerService();
-        brokerService.addConnector("tcp://localhost:61616");
-        brokerService.setBrokerName("test broker instance");
-        brokerService.start();
-        System.out.println("Started Embedded Broker");
-        return brokerService;
+    public static void clientAckCustomer() throws JMSException {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+            Destination destination = session.createQueue("client ack queue");
+            MessageConsumer consumer = session.createConsumer(destination);
+            TextMessage message = (TextMessage) consumer.receive();
+            System.out.println(message.getText());
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
     }
-     */
+
+    public static void dupsOkAckCustomer() throws JMSException {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.DUPS_OK_ACKNOWLEDGE);
+            Destination destination = session.createQueue("dups ok ack queue");
+            MessageConsumer consumer = session.createConsumer(destination);
+            TextMessage message = (TextMessage) consumer.receive();
+            System.out.println(message.getText());
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
+    }
+
+    public static void sessionTransactedAckCustomer() throws JMSException {
+        try (Connection connection = getConnection()) {
+            Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
+            Destination destination = session.createQueue("session transacted ack queue");
+            MessageConsumer consumer = session.createConsumer(destination);
+            TextMessage message = (TextMessage) consumer.receive();
+            System.out.println(message.getText());
+        } catch (JMSException jmsException) {
+            //add log4j error record
+        }
+    }
 }
